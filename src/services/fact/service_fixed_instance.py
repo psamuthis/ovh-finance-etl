ce9ed4debafebf9670dcd6bd5a0b15cfa4cded12
Rousseau Emile <emilerousseau16@gmail.com>
2026-06-15 09:56:00 +0200

    corrected month end calculation

--- src/services/fact/service_fixed_instance.py
+++ src/services/fact/service_fixed_instance.py
@@ -3,7 +3,8 @@ from datetime import datetime, timezone
 from models.dimension.dim_time import DimTime
 from models.fact.fact_current_fixed_compute import FactCurrentFixedCompute
 from services.db_service import DBService
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, aliased
+from dateutil.relativedelta import relativedelta
 
 
 class ServiceFixedInstance(DBService):
@@ -12,7 +13,7 @@ class ServiceFixedInstance(DBService):
 
     def get_or_create(self, record: FactCurrentFixedCompute) -> int:
         month_start: datetime = datetime.now(timezone.utc).replace(day=1, minute=0, second=0, microsecond=0)
-        month_end: datetime = month_start.replace(month=month_start.month+1)
+        month_end: datetime = month_start + relativedelta(months=1)
 
         existing_record: FactCurrentFixedCompute  = self.db.query(FactCurrentFixedCompute)\
             .filter(FactCurrentFixedCompute.instance_id==record.instance_id)\
