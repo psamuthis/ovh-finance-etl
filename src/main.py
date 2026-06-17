e87b4e86b9e586381c0cf12eac2db4e58701fe50
Rousseau Emile <emilerousseau16@gmail.com>
2026-06-17 11:43:15 +0200

    added exclude of finops and provision

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+from enum import Enum
+
 from etl.etl import ETL
 from models.raw.current_usage_raw import CurrentUsageRaw
 from connector.postgres_connection import RawSessionLocal, WarehouseSessionLocal
 from services.raw.service_current_usage_raw import ServiceUsageRaw
+
+from excluded_tenants import EXCLUDED_TENANTS
+
+EXCLUDED_TENANT_IDS: set[str] = set(EXCLUDED_TENANTS.values())
 
 with RawSessionLocal() as db:
     latest_records: list[CurrentUsageRaw] = ServiceUsageRaw(db).retrieve_latest_data()
@@ -10,4 +16,7 @@ if latest_records is None:
     raise ValueError("Latest raw record wasn't retrieved...")
 
 for record in latest_records:
+    if record.service_id in EXCLUDED_TENANT_IDS:
+        continue
+
     ETL(record).run()
