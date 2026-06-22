6cdd8214f2233c35f0b6e7c275f93243caeb91d5
Rousseau Emile <emilerousseau16@gmail.com>
2026-06-22 10:01:01 +0200

    updated db connector for env changes.

--- src/connector/db_engine.py
+++ src/connector/db_engine.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-RAW_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_RAW_PORT')}/{os.getenv('DB_RAW_NAME')}"
-WRHS_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_WRHS_PORT')}/{os.getenv('DB_WRHS_NAME')}"
+RAW_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_RAW_USER')}:{os.getenv('DB_RAW_PWD')}@{os.getenv('DB_RAW_HOST')}:{os.getenv('DB_RAW_PORT')}/{os.getenv('DB_RAW_NAME')}"
+WRHS_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_WRHS_USER')}:{os.getenv('DB_wRHS_PWD')}@{os.getenv('DB_WRHS_HOST')}:{os.getenv('DB_WRHS_PORT')}/{os.getenv('DB_WRHS_NAME')}"
 
 
 def get_raw_engine() -> Engine:
