3c54f733db79fda85e820b419dda57edce10710d
Rousseau Emile <emilerousseau16@gmail.com>
2026-07-06 11:00:32 +0200

    added connection string and method for mysql finops

--- src/connector/db_engine.py
+++ src/connector/db_engine.py
@@ -4,13 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-RAW_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_RAW_NAME')}?sslmode=require"
-WRHS_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_WRHS_NAME')}?sslmode=require"
+RAW_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_RAW_NAME')}"
+WRHS_CONN_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_WRHS_NAME')}"
+FINOPS_MYSQL = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PWD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB_NAME')}"
 
 
 def get_raw_engine() -> Engine:
     return create_engine(RAW_CONN_URL)
 
-
 def get_warehouse_engine() -> Engine:
     return create_engine(WRHS_CONN_URL)
+
+def get_finops_mysql_engine() -> Engine:
+    return create_engine(FINOPS_MYSQL)
